20121e6a1cb1a9632d39c6f3e538676ef0ca772b
vitor almeida <vitormatheus80@gmail.com>
2021-10-04 15:09:48 -0300

    exercicios do dia 04/10/2021

---
+++ Exercicios-04-10-2021/src/br/com/generation/exercicios/Exercicio02.java
@@ -0,0 +1,44 @@
+package br.com.generation.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int a,b,c;
+		
+		System.out.println("Digite o primeiro numero: ");
+		a=leia.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		b=leia.nextInt();
+
+		System.out.println("Digite o terceiro numero: ");
+		c=leia.nextInt();
+
+		if(a>=b && b>=c) {
+			System.out.println(a + "-" + b + "-" + c);
+		}
+		else if(a>c && c>b) {
+			System.out.println(a + "-" + c + "-" + b);
+		}
+		
+		else if(b>a && a>c) {
+			System.out.println(b + "-" + a + "-" + c);
+		}
+		else if(b>c && c>a) {
+			System.out.println(b + "-" + c + "-" + a);
+		}
+		else if (c>a && a>b) {
+			System.out.println(c + "-" + a + "-" + b);
+		}
+		else if(c>b && b>a) {
+			System.out.println(c + "-" + b + "-" + a);
+		}
+		leia.close();
+	}
+
+}
